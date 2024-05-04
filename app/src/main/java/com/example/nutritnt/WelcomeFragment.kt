44e2b0cff7027db5e78fbeebb64f08e0b2a85e91
nicole-alvarado <alvaradonicole123@hotmail.com>
2024-05-03 22:07:09 -0300

    agregado código corregido en WelcomeFragment

--- app/src/main/java/com/example/nutritnt/WelcomeFragment.kt
+++ app/src/main/java/com/example/nutritnt/WelcomeFragment.kt
@@ -15,42 +15,33 @@ import com.example.nutritnt.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
-
         private lateinit var binding: FragmentWelcomeBinding
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-
-            //setContentView(R.layout.activity_welcome)
-
-            binding = FragmentWelcomeBinding.inflate(layoutInflater)
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflar el diseño del fragmento utilizando el FragmentWelcomeBinding
+            binding = FragmentWelcomeBinding.inflate(inflater, container, false)
             val view = binding.root
 
-
             binding.buttonEncuestas.text = "Ver encuestas"
-            binding.textBienvenida.text = "Binvenido/a"
+            binding.textBienvenida.text = "Bienvenido/a"
 
-            binding.buttonEncuestas.setOnClickListener(){
-                Log.d("Botones", "Botón ver encuentas clickeado")
-                val context = binding.root.context
+            binding.buttonEncuestas.setOnClickListener {
+                Log.d("Botones", "Botón ver encuestas clickeado")
+                val context = requireContext()
                 val intent = Intent(context, ListadoEncuestasActivity::class.java)
-                context.startActivity(intent) // Iniciar la actividad con el intent
+                context.startActivity(intent)
             }
+            
+            // Aplicar los insets del sistema a la vista
             ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
             }
 
+            return view
+        }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
-
-
-}
