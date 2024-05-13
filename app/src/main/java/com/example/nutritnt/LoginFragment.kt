c34222f7aa227f5aa121f42abe0aabc6909b16c1
nicole-alvarado <alvaradonicole123@hotmail.com>
2024-05-13 15:51:26 -0300

    se borraron comentarios de imports no usados

--- app/src/main/java/com/example/nutritnt/LoginFragment.kt
+++ app/src/main/java/com/example/nutritnt/LoginFragment.kt
@@ -1,6 +1,4 @@
 package com.example.nutritnt
-
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,10 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.example.nutritnt.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
