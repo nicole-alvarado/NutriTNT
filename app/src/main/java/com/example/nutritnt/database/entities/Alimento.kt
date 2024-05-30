0568267e38466457bb771fbac1893fd8fe169576
nicole-alvarado <alvaradonicole123@hotmail.com>
2024-05-30 20:46:08 -0300

    agregados atributos nuevos al modelo Alimento

--- app/src/main/java/com/example/nutritnt/database/entities/Alimento.kt
+++ app/src/main/java/com/example/nutritnt/database/entities/Alimento.kt
@@ -2,9 +2,15 @@ package com.example.nutritnt.database.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tabla_alimento")
+@Entity(tableName = "tabla_alimento",foreignKeys =
+[
+    ForeignKey(entity = InformacionNutricional::class,
+        parentColumns = ["informacionNutricionalId"],
+        childColumns = ["informacionNutricionalId"],
+        onDelete = ForeignKey.CASCADE)])
 data class Alimento (
     @PrimaryKey(autoGenerate = true)
     var alimentoId: Int = 0,
@@ -17,4 +23,16 @@ data class Alimento (
 
     @ColumnInfo(name = "grasas_totales")
     var grasas_totales: Float,
+
+    @ColumnInfo(name = "codigo")
+    var codigo: String,
+
+    @ColumnInfo(name = "grupo")
+    var grupo: String,
+
+    @ColumnInfo(name = "subgrupo")
+    var subgrupo: String,
+
+    @ColumnInfo(name = "informacionNutricionalId")
+    var informacionNutricionalId: Int,
 )
