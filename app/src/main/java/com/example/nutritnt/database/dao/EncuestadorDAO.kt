125b378b360a79617ebcd03ab99cdc60680824df
nicole-alvarado <alvaradonicole123@hotmail.com>
2024-05-30 19:46:45 -0300

    agregado notacion Dao

--- app/src/main/java/com/example/nutritnt/database/dao/EncuestadorDAO.kt
+++ app/src/main/java/com/example/nutritnt/database/dao/EncuestadorDAO.kt
@@ -1,11 +1,13 @@
 package com.example.nutritnt.database.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.nutritnt.database.entities.Encuestador
 
+@Dao
 interface EncuestadorDAO {
     @Query("SELECT * from tabla_encuestador ORDER BY encuestadorId ASC")
     fun getEncuestadores(): LiveData<List<Encuestador>>
