aa8786238d4bc87e0e6901de30dfc5a9b524e442
nicole-alvarado <alvaradonicole123@hotmail.com>
2024-05-30 19:31:42 -0300

    agregado dao para Encuestador

---
+++ app/src/main/java/com/example/nutritnt/database/dao/EncuestadorDAO.kt
@@ -0,0 +1,28 @@
+package com.example.nutritnt.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.nutritnt.database.entities.Encuestador
+
+interface EncuestadorDAO {
+    @Query("SELECT * from tabla_encuestador ORDER BY encuestadorId ASC")
+    fun getEncuestadores(): LiveData<List<Encuestador>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertar(encuestador: Encuestador)
+
+    @Query("DELETE FROM tabla_encuestador")
+    suspend fun borrarTodos()
+
+    @Query("SELECT COUNT(encuestadorId) from tabla_encuestador")
+    suspend fun cantidadDeEncuestadores(): Int
+
+    @Query("""
+        SELECT e.*
+        FROM tabla_encuestador e
+        WHERE e.encuestadorId = :id
+    """)
+    fun getEncuestadorById(id: Int): LiveData<Encuestador>
+}
